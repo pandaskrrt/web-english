c37f6270767db716c3f35305efdb792d142c3de9
Riyan Fanani <ogickarecka1108@gmail.com>
2023-10-15 18:04:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+This template provides a minimal setup to get React working in Vite with Typescript and Shadcn UI
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { FC } from 'react'
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Button className="bg-red-400">Test</Button>
+    </div>
+  )
+}
+
+export default Home
