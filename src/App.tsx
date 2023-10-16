c515153229b5c187e491e350d2f81797181418d9
Riyan Fanani <ogickarecka1108@gmail.com>
2023-10-16 11:41:46 +0700

    add intership pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Intership from './pages/Intership'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/intership" element={<Intership />} />
     </Routes>
   )
 }
